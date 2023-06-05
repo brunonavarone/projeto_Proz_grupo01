9c6a370f1e901cdb4c0b4547018e839cb42a2ca7
Érika Bedin <erikabedin.administradora@gmail.com>
2023-06-05 17:52:05 -0300

    Alterações Sprint IV Érika

--- scripts/renderizacao.js
+++ scripts/renderizacao.js
@@ -3,14 +3,21 @@ const videos = [
     "https://www.youtube.com/embed/6NXnxTNIWkc?autoplay=1&cc_load_policy=1&controls=1&enablejsapi=1&rel=0",
     "https://www.youtube.com/embed/Lo2qQmj0_h4?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0",
     "https://www.youtube.com/embed/JkK8g6FMEXE?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0",
-    "https://www.youtube.com/embed/TAqZb52sgpU?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0"
+    "https://www.youtube.com/embed/TAqZb52sgpU?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0",
+    "https://www.youtube.com/embed/9BMwcO6_hyA?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0",
+    "https://www.youtube.com/embed/CD-E-LDc384?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0",
+    "https://www.youtube.com/embed/8SbUC-UaAxE?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0",
+    "https://www.youtube.com/embed/bWXazVhlyxQ?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0",
+    "https://www.youtube.com/embed/rn_YodiJO6k?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0",
+    "https://www.youtube.com/embed/egMWlD3fLJ8?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0",
+    "https://www.youtube.com/embed/6Ejga4kJUts?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0",
+    "https://www.youtube.com/embed/nrIPxlFzDi0?autoplay=0&cc_load_policy=1&controls=1&enablejsapi=1&rel=0"
   ];
   
   // Função para renderizar os vídeos
   function renderizarVideos() {
     const videosContainer = document.getElementById("videos-container");
-    console.log renderizarVideos
-
+    
     // Limpar conteúdo existente
     videosContainer.innerHTML = "";
   
